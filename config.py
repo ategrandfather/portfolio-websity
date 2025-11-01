e6d4a8cf9c4c0bd5a7ebe05c453d246241433cc7
ategrandfather <mflentin@gmail.com>
2025-11-01 09:16:53 +0000

    Fix config loading

--- config.py
+++ config.py
@@ -3,8 +3,6 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
@@ -13,7 +11,11 @@ class Config:
 
     SQLALCHEMY_DATABASE_URI = os.getenv(
         "DATABASE_URL",
-        "sqlite:///" + os.path.join(BASEDIR, "app.db"),
+        "sqlite:///app.db",
     )
-
+    print(SQLALCHEMY_DATABASE_URI)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+
+aa = Config()
+print(aa)
